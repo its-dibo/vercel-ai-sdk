66f17ca2052a652d72b8f0dcc54c3021f5a67027
its-dibo <sh.eldeeb.2010@gmail.com>
2026-02-28 05:12:01 +0200

    setup AI SDK

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,18 @@
+import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+
+ const openrouter = createOpenRouter({
+    apiKey: process.env.OPENROUTER_API_KEY,
+  });
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+   model: openrouter("openrouter/free"),
+    messages: await convertToModelMessages(messages),
+  });
+
+  return result.toUIMessageStreamResponse();
+}
+
