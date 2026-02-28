46a0fdec9da358fc27f269495b243bdbbd50fcb1
its-dibo <sh.eldeeb.2010@gmail.com>
2026-02-28 08:38:23 +0200

    add AI devtools

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,17 +1,29 @@
-import { streamText, UIMessage, convertToModelMessages, tool, stepCountIs } from 'ai';
-import { createOpenRouter } from '@openrouter/ai-sdk-provider';
-import { z } from 'zod';
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  tool,
+  stepCountIs,
+  wrapLanguageModel,
+} from "ai";
+import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+import { z } from "zod";
+import { devToolsMiddleware } from "@ai-sdk/devtools";
 
+const openrouter = createOpenRouter({
+  apiKey: process.env.OPENROUTER_API_KEY,
+});
 
- const openrouter = createOpenRouter({
-    apiKey: process.env.OPENROUTER_API_KEY,
-  });
+const model = wrapLanguageModel({
+  model: openrouter("openrouter/free"),
+  middleware: devToolsMiddleware(),
+});
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-   model: openrouter("openrouter/free"),
+    model,
     messages: await convertToModelMessages(messages),
     stopWhen: stepCountIs(5),
     onStepFinish: ({ toolResults }) => {
@@ -19,9 +31,9 @@ export async function POST(req: Request) {
     },
     tools: {
       weather: tool({
-        description: 'Get the weather in a location (celsius)',
+        description: "Get the weather in a location (celsius)",
         inputSchema: z.object({
-          location: z.string().describe('The location to get the weather for'),
+          location: z.string().describe("The location to get the weather for"),
         }),
         execute: async ({ location }) => {
           const temperature = 25;
@@ -36,4 +48,3 @@ export async function POST(req: Request) {
 
   return result.toUIMessageStreamResponse();
 }
-
