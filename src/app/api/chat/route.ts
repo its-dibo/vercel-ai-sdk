48964869774b21c509f6b7d3209d8be2ccc2a463
its-dibo <sh.eldeeb.2010@gmail.com>
2026-02-28 05:28:06 +0200

    add weather tool

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,7 @@
-import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { z } from 'zod';
+
 
  const openrouter = createOpenRouter({
     apiKey: process.env.OPENROUTER_API_KEY,
@@ -11,6 +13,21 @@ export async function POST(req: Request) {
   const result = streamText({
    model: openrouter("openrouter/free"),
     messages: await convertToModelMessages(messages),
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location (celsius)',
+        inputSchema: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          const temperature = 25;
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+    },
   });
 
   return result.toUIMessageStreamResponse();
